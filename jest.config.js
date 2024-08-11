4279c5e1df3ff47bbcfb977f35a36f64a5b2d9f5
Thiago Piacentini <126130796+piacentinithiago@users.noreply.github.com>
2024-08-11 23:41:07 +0800

    chore: install next, jest, style-dictionary

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+};
